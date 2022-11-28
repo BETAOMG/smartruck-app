7ffba34aef8a04cb4cc23270cbefed1f3000df9d
BETAOMG <ldbetancourt5@misena.edu.co>
2022-11-28 12:32:48 -0500

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,132 @@
+<div class="container-logo">
+    <img src="./assets/images/logo-dark.svg" alt="Smarttruck">
+</div>
+
+<nav class="navbar nav-bar-container" role="navigation" aria-label="main navigation" >
+
+    <div class="navbar-brand">
+        <div class="navbar-burger" data-target="navbarExampleTransparentExample">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+
+    <div id="navbarExampleTransparentExample" class="navbar-menu ">
+        <a class="navbar-item" href="./inicio.html">
+            <img src="./assets/logo/home-logo.svg" width="" >
+        </a>
+
+        <div class="navbar-start">
+
+            <div class="navbar-item has-dropdown is-hoverable">
+
+                <a class="navbar-link has-background-link-light">
+                    Gestionar
+                </a>
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="./usuarios.html">
+                        Usuarios
+                    </a>
+
+                    <a class="navbar-item" href="./proveedores.html">
+                        Proveedores
+                    </a>
+
+                    <a class="navbar-item" href="./vehiculos.html">
+                        Vehiculos
+                    </a>
+                </div>
+
+            </div>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link has-background-link-light">
+                    Mantenimiento
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="./mantenimiento.html">
+                        Gestionar mantenimiento
+                    </a>
+                </div>
+
+            </div>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link has-background-link-light">
+                    Documentacion
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="./documentacion.html">
+                        Gestionar documentacion
+                    </a>
+                </div>
+            </div>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link has-background-link-light">
+                    Normativas
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="./normativa.html">
+                        Ver normativas
+                    </a>
+                </div>
+
+            </div>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link has-background-link-light">
+                    Rutas
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="./satrack.html">
+                        Satrack
+                    </a>
+                    <a class="navbar-item" href="./peajes.html">
+                        Peajes
+                    </a>
+                    <a class="navbar-item" href="./cierrevial.html">
+                        Cierre vial
+                    </a>
+
+                </div>
+            </div>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link has-background-link-light">
+                    Atencion al cliente
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a class="navbar-item" href="./atencionalcliente.html">
+                        Preguntas,  reclamos, sugerencias
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="navbar-end">
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class="navbar-link" href="#">
+                    <img class="logo-perfil" src="./assets/logo/github-logo.svg" alt="" >
+                </a>
+                <div class="navbar-dropdown is-boxed">
+                    <a class="navbar-item" href="#">
+                        Ver perfil
+                    </a>
+
+                    <hr class="navbar-divider">
+
+                    <a class="navbar-item is-active" href="./inicio-sesion.html">
+                        Cerrar sesion
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
